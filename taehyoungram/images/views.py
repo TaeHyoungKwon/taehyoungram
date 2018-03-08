5d34475149233e61cfa1a9f7f9a7c1798d011fe6
TaeHyoungKwon <kth5604@gmail.com>
2018-03-08 23:11:38 +0900

    Creating the URLS and test the Image Serializers

--- taehyoungram/images/views.py
+++ taehyoungram/images/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.response import response
+from rest_framework.response import Response
 from .models import Image
 from .serializers import ImageSerializer
 
@@ -8,5 +8,5 @@ class ListAllImages(APIView):
     def get(self, request, format=None):
         
         all_images = Image.objects.all()
-        serializer = serializers.ImageSerializer(all_images, many=True)
+        serializer = ImageSerializer(all_images, many=True)
         return Response(data=serializer.data)
